125c39d31a73d0f4b4bfbd696ba573ced37797c8
Ivan Ivanov <ivanov399@mail.ru>
2022-10-13 18:18:55 +0300

    add Zadacha50

--- Zadacha47/Program.cs
+++ Zadacha47/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака. m = 3, n = 4.
+﻿// Zadacha47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака. m = 3, n = 4.
+
 Console.WriteLine("введите число строк");
 int line = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число столбцов");
@@ -7,30 +8,30 @@ double[,] numbers = new double[line, colum];
 FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
 
-    void FillArrayRandomNumbers(double[,] array)
-    {
-        Random random = new Random();
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i, j] = Convert.ToDouble(random.Next(-10, 10) + random.NextDouble());
-            }
-        }
-    }
+void FillArrayRandomNumbers(double[,] array)
+{
+	Random random = new Random();
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			array[i, j] = Convert.ToDouble(random.Next(-10, 10) + random.NextDouble());
+		}
+	}
+}
 
-    void PrintArray(double[,] array)
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            Console.Write("[ ");
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                Console.Write(Math.Round(array[i, j],1) + " ");
-            }
-            Console.Write("]");
-			Console.WriteLine();
-           
-        }
-    }
-    
+void PrintArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		Console.Write("[ ");
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write(Math.Round(array[i, j], 1) + " ");
+		}
+		Console.Write("]");
+		Console.WriteLine();
+
+	}
+}
+
